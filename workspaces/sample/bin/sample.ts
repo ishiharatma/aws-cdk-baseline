04417bd8f434586533751db18ba25770be89cff7
ishr <ishr1629@gmail.com>
2024-04-04 11:58:25 +0900

    update

--- workspaces/sample/bin/sample.ts
+++ workspaces/sample/bin/sample.ts
@@ -4,6 +4,38 @@ import * as cdk from 'aws-cdk-lib';
 import { SampleStack } from '../lib/stacks/sample-stack';
 
 const app = new cdk.App();
+
+// 文字色
+const color_red: string = '\u001b[31m';
+const color_green: string = '\u001b[32m';
+const color_yellow: string = '\u001b[33m';
+const color_white: string = '\u001b[37m';
+const color_reset: string = '\u001b[0m';
+
+// 環境識別子の指定
+const envname: string = app.node.tryGetContext('env');
+
+console.log();
+console.log(`${color_yellow}##########################################${color_reset}`);
+console.log(`${color_yellow}  sample プロジェクト${color_reset}`);
+console.log(`${color_yellow}  リリース環境：${color_reset} ${color_red}${envname}${color_reset}`);
+console.log(`${color_yellow}##########################################${color_reset}`);
+console.log();
+
+// 環境識別子のチェック
+if (!envname.match(/^(dev|test|stage|prod|jump)$/)) {
+  console.warn('Invalid context. envname must be [dev , test, stage, prod, jump].');
+  process.exit(1);
+}
+
+const isProduction:boolean = envname.match(/^(prod)$/) ? true: false;
+if (isProduction) {
+  console.log(`${color_red}!!!!!!!!!! CAUTION !!!!!!!!!!${color_reset}`);
+  console.log(`${color_red}   本番環境へのリリースです。${color_reset}`);
+  console.log(`${color_red}!!!!!!!!!! CAUTION !!!!!!!!!!${color_reset}`);
+};
+
+
 new SampleStack(app, 'SampleStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
